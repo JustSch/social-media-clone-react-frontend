86c87457ec9e7303c62672d523107bbe27087c15
Justin Schreiber <jschreiber234@gmail.com>
2023-04-26 22:14:21 -0400

    login page cannow use login api

--- pages/users/login.js
+++ pages/users/login.js
@@ -1,7 +1,42 @@
 import Link from "next/link";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { useState } from "react";
+import { Col, Container, Row, Card, Form, FormControl, Button, Alert } from "react-bootstrap";
 
 const Login = () => {
+    const [errorMsg, setErrorMsg] = useState('')
+
+     async function handleSubmit(e){
+        e.preventDefault();
+        if (errorMsg) setErrorMsg('');
+        try {
+            const formData = new FormData();
+            formData.append("email",e.currentTarget.email.value);
+            formData.append("password",e.currentTarget.password.value);
+            const data = new URLSearchParams(formData);
+            const res = await fetch('/api/login',{
+                method: 'POST',
+                body: data,
+                credentials: 'include'
+
+            });
+
+            if (res.status === 200){
+                setErrorMsg('login successful')//change to redirect with Router.push('/')
+            }
+            else if (res.status === 401){
+                setErrorMsg('email or password is incorrect')
+            }
+            else {
+                setErrorMsg('an unknown error has occured while logging in')
+            }
+        }
+
+        catch(error){
+            console.log(error.message);
+            setErrorMsg(error.message)
+        }
+    }
+
     return (
         <div>
             <Container>
@@ -9,32 +44,33 @@ const Login = () => {
                     <Col className="col-md-6 m-auto">
                         <Card className='card-body'>
                             <h1 className="text-center mb-3">Login</h1>
-
-                            <form action="/api/users/login" method="POST">
-                                <div className="form-group">
-                                    <label htmlFor="email" className="form-label mt-4">Email</label>
-                                    <input
+                            {errorMsg && <Alert className="alert-warning">{errorMsg}</Alert>} 
+                            <Form onSubmit={handleSubmit}>
+                                <Form.Group>
+                                    <Form.Label htmlFor="email" className="mt-4">Email</Form.Label>
+                                    <FormControl
                                         type="email"
                                         id="email"
                                         name="email"
                                         className="form-control"
                                         placeholder="Enter Email"
                                     />
-                                </div>
-                                <div className="form-group">
-                                    <label htmlFor="password" className="form-label mt-4">Password</label>
-                                    <input
+                                </Form.Group>
+                                <Form.Group>
+                                    <Form.Label htmlFor="password" className="mt-4">Password</Form.Label>
+                                    <FormControl
                                         type="password"
                                         id="password"
                                         name="password"
                                         className="form-control"
                                         placeholder="Enter Password"
                                     />
-                                </div>
+                                </Form.Group>
                                 <div className="d-grid gap-2">
-                                    <button type="submit" className="btn btn-large btn-primary mt-4">Login</button>
+                                    <Button type="submit" className="btn-large btn-primary mt-4">Login</Button>
                                 </div>
-                            </form>
+
+                            </Form>
 
                             <p className="lead mt-4">
                                 No Account? <Link href="/users/register">Register</Link>
