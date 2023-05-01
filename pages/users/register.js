aaa57814117fc7d969b3f2809054fca25e6a0fc9
Justin Schreiber <jschreiber234@gmail.com>
2023-05-01 14:50:07 -0400

    added client side register validation

--- pages/users/register.js
+++ pages/users/register.js
@@ -1,18 +1,32 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { Card, Col, Form, FormControl, FormGroup, FormLabel, Row, Container, Button, Alert } from "react-bootstrap";
+import { Card, Col, Form, FormControl, Row, Container, Button, Alert } from "react-bootstrap";
 
 const Register = () => {
     const [errorMsg, setErrorMsg] = useState('');
     let router = useRouter();
 
-    const submitHandler = async (e) => {
+    async function handleSubmit (e) {
         e.preventDefault();
+        let errors=[];
         if (errorMsg) setErrorMsg('');
+        if (!e.currentTarget.name.value ||
+            !e.currentTarget.email.value ||
+            !e.currentTarget.password.value ||
+            !e.currentTarget.password2.value) {
+                errors.push("Please Enter All Fields!!");
+        }
 
         if (e.currentTarget.password.value !== e.currentTarget.password2.value){
-            setErrorMsg("The Passwords Do Not Match Please Try Again");
+            errors.push("The Passwords Do Not Match Please Try Again");
+        }
+
+        if (e.currentTarget.password.value < 6){
+            errors.push("Password must be at least 6 characters");
+        }
+        if (errors.length > 0) {
+            setErrorMsg(errors.join(' '));
         }
 
         else {
@@ -20,7 +34,7 @@ const Register = () => {
         }
         
 
-    };
+    }
     return (
         <Container>
             <Row className="mt-5">
@@ -30,54 +44,50 @@ const Register = () => {
                             Register
                         </h1>
                         {errorMsg && <Alert className="alert-warning">{errorMsg}</Alert>}
-                        <Form onSubmit={submitHandler}>
-                            <FormGroup>
-                                <FormLabel htmlFor="name" className="mt-4">
+                        <Form onSubmit={handleSubmit}>
+                            <Form.Group>
+                                <Form.Label htmlFor="name" className="mt-4">
                                     Name
-                                </FormLabel>
+                                </Form.Label>
                                 <FormControl type="name"
                                     id="name"
                                     name="name"
-                                    class="form-control"
                                     placeholder="Enter Name"
                                 />
-                            </FormGroup>
-                            <FormGroup>
-                                <FormLabel htmlFor="email" className="mt-4">
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="email" className="mt-4">
                                     Email
-                                </FormLabel>
+                                </Form.Label>
                                 <FormControl
                                     type="email"
                                     id="email"
                                     name="email"
-                                    class="form-control"
                                     placeholder="Enter Email"
                                 />
-                            </FormGroup>
-                            <FormGroup>
-                                <FormLabel htmlFor="password" className="mt-4">
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="password" className="mt-4">
                                     Password
-                                </FormLabel>
+                                </Form.Label>
                                 <FormControl
                                     type="password"
                                     id="password"
                                     name="password"
-                                    class="form-control"
                                     placeholder="Create Password"
                                 />
-                            </FormGroup>
-                            <FormGroup>
-                                <FormLabel htmlFor="password2" className="mt-4">
+                            </Form.Group>
+                            <Form.Group>
+                                <Form.Label htmlFor="password2" className="mt-4">
                                     Confirm Password
-                                </FormLabel>
+                                </Form.Label>
                                 <FormControl
                                     type="password"
                                     id="password2"
                                     name="password2"
-                                    class="form-control"
                                     placeholder="Confirm Password"
                                 />
-                            </FormGroup>
+                            </Form.Group>
                             <div className="d-grid gap-2">
                                 <Button type="submit" className="btn-large btn-primary mt-4">Register</Button>
                             </div>
