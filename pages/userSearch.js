c13f32a2bea3de1e0b2942785140f72f4991dd97
Justin Schreiber <jschreiber234@gmail.com>
2023-05-02 14:09:54 -0400

    added error message for user search

--- pages/userSearch.js
+++ pages/userSearch.js
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
 import { useState } from "react";
-import { Button, Card, Col, Container, Form, InputGroup, Row } from "react-bootstrap";
+import { Button, Card, Col, Container, Form, InputGroup, Row, Alert } from "react-bootstrap";
 import { createResult } from '@/components/searchResult'
 
 const UserSearch = () => {
     let router = useRouter();
     const [errorMsg, setErrorMsg] = useState('');
     const [searchVal,setSearchVal] = useState('');
-    const [resultJSON,setResultHTML] = useState('');
+    const [resultJSON,setResultJSON] = useState('');
 
     //create another react component to create search results
     //links are created based on username
@@ -24,9 +24,9 @@ const UserSearch = () => {
                     method: 'GET'
                 });
                 const data = await res.json();
-                setResultHTML(data);
+                if (data.length > 0) setResultJSON(data);
+                else  setErrorMsg("No Posts Could Be Found For This User");
                 
-
             }
 
             catch (error) {
@@ -62,6 +62,7 @@ const UserSearch = () => {
                                 Search
                             </Button>
                         </InputGroup>
+                        {errorMsg && <Alert className="mt-2 alert-warning">{errorMsg}</Alert>}
                         {resultJSON && createResult(resultJSON)}
                     </Card>
                 </Col>
