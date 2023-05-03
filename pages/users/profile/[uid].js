f18f5094306405dc5c7caddd899554b35f73144c
Justin Schreiber <jschreiber234@gmail.com>
2023-05-03 18:47:35 -0400

    added ability to follow and unfollow

--- pages/users/profile/[uid].js
+++ pages/users/profile/[uid].js
@@ -1,18 +1,21 @@
 import { useJSONFetcher } from '@/hooks/JSONFetcher';
+import { useAuthenticator } from '@/hooks/authenticated';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from "react";
 import { Card, Col, Button, Row } from 'react-bootstrap';
 
 const Profile = () => {
+  const authenticated = useAuthenticator();
   const router = useRouter();
   const { uid } = router.query;
   const [errorMsg, setErrorMsg] = useState('');
   const [headerHTML, setHeaderHTML] = useState('');
   const [postHTML, setPostHTML] = useState('');
 
-  //fetch to get user info to create header(make a seperate hook)
-  //fetch to get user's posts to make post section(make a seperate hook)
+  const [btnText, setBtnText] = useState('Login To Follow User');
+  const [btnHref, setBtnHref] = useState('/users/login');
+
 
 
   useEffect(() => {
@@ -25,25 +28,89 @@ const Profile = () => {
         return;
       }
       let data = await res.json();
-      setHeaderHTML(header_markup(data))
-      
+      setHeaderHTML(header_markup(data, btnText));
+
       fetchingUserData();
     }
     const dataUrl = `/api/user/${uid}/posts`;
     let fetchingUserData = async () => {
       let res = await fetch(dataUrl);
-      
+
       let data = await res.json();
-      if (res.status === 404 || data.length === 0){
+      if (res.status === 404 || data.length === 0) {
         setErrorMsg('This User Does Not Have Any Posts');
         return;
       }
 
       setPostHTML(post_markup(data));
     }
-
     fetchingUser();
-  }, [uid])
+    if (!authenticated) return;
+    let followedStatus = async () => {
+      let res = await fetch(`/api/isFollowing/${uid}`);
+
+      let data = await res.json();
+
+      if (res.status === 404) {
+        setErrorMsg(data.msg);
+        return;
+      }
+
+      else {
+        if (data.isFollowing) {
+          setBtnText('Unfollow');
+          setBtnHref(`/api/user/unfollow/`);
+        }
+        else {
+          setBtnText('Follow');
+          setBtnHref(`/api/user/follow/`);
+        }
+      }
+
+    }
+    followedStatus();
+
+  }, [uid, authenticated, btnHref, btnText])
+
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('username',uid);
+    const data = new URLSearchParams(formData);
+    let res = await fetch(btnHref,{
+      method: 'POST',
+      credentials: 'include',
+      body: data
+    });
+  
+    if (res.status === 200) {
+      if (btnText === 'Follow'){
+        setBtnText('Unfollow');
+        setBtnHref(`/api/user/unfollow/`);
+      }
+      else {
+        setBtnText('Follow');
+        setBtnHref(`/api/user/follow/`);
+      }
+    }
+  }
+
+  const header_markup = (user, btnText) => {
+    return (
+      <Card>
+        <Card.Body>
+          <h1 className='card-title'>
+            {`${user.name}'s Profile`}
+          </h1>
+          <h6 className="card-subtitle mb-2 text-muted">
+            {`Following: ${user.following.length}  Followers: ${user.followers.length}`}
+            <Button type="button" className="btn-outline float-end" id="follow_btn" onClick={handleClick}>{btnText}</Button>
+          </h6>
+        </Card.Body>
+      </Card>
+    )
+  }
 
 
   return (
@@ -93,30 +160,18 @@ const post_markup = (postJSON) => {
   )
 }
 
-const header_markup = (user) => {
-  return (
-    <Card>
-      <Card.Body>
-        <h1 className='card-title'>
-          {`${user.name}'s Profile`}
-        </h1>
-        <h6 className="card-subtitle mb-2 text-muted">
-          {`Following: ${user.following.length}  Followers: ${user.followers.length}`}
-          <Button type="button" className="btn-outline float-end" id="follow_btn">Follow</Button>
-        </h6>
-      </Card.Body>
-    </Card>
-  )
-}
+
 
 const error_markup = (error_message) => {
   return (
     <Row className="mt-5">
-        <Col className="col-md-6 m-auto">
-          <Card className="card-body">
+      <Col className="col-md-6 m-auto">
+        <Card className="card-body">
           <h1 className="text-center mb-3">{error_message}</h1>
-          </Card>
-        </Col>
-      </Row>
+        </Card>
+      </Col>
+    </Row>
   )
 }
+
+
